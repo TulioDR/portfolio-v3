bc57f8227ebb4ae19234a6eb64bc906250260b00
Tulio <tulioruzo29@gmail.com>
2023-04-06 19:14:24 -0400

    Add shield Logo to About Card Mobile

--- src/components/Sections/About/ShieldLogo.tsx
+++ src/components/Sections/About/ShieldLogo.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function ShieldLogo({}: Props) {
    return (
-      <div className="card-logo mx-auto aspect-[9/16] w-52 p-2 bg-gradient-to-b from-red-900 to-yellow-700 rounded-2xl shadow-xl">
+      <div className="card-logo mx-auto aspect-[9/16] w-2/3 sm:w-52 p-2 bg-gradient-to-b from-red-900 to-yellow-700 rounded-2xl shadow-xl">
          <div className="bg-main-primary w-full h-full rounded-2xl grid place-content-center p-5">
             <Image src={shield} alt="shield" className="w-full" />
          </div>
--- src/components/Sections/About/mobile/AboutCardMobile.tsx
+++ src/components/Sections/About/mobile/AboutCardMobile.tsx
@@ -1,5 +1,6 @@
 import useLanguageContext from "@/context/LanguageContext";
 import React from "react";
+import ShieldLogo from "../ShieldLogo";
 
 export default function AboutCardMobile() {
    const { currentLanguage } = useLanguageContext();
@@ -7,6 +8,7 @@ export default function AboutCardMobile() {
    return (
       <div className="w-full rounded-3xl bg-main-primary text-main-white font-semibold text-xl p-5 sm:p-10 md:p-14 mt-10">
          <div className="w-40">{card}</div>
+         <ShieldLogo />
       </div>
    );
 }
